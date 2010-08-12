a3714a70450168e6ea3954073a2bc63b347bbd1a
robert.alexandersson@gmail.com <robert.alexandersson@gmail.com@81990c2f-af16-a326-1b66-97b0f14e0d32>
2010-08-12 19:23:44 +0000

    renamed module to avoid Maven test suite autodetection

--- trunk/transactions/src/test/java/test/com/hrodberaht/inject/extension/transaction/example/ModuleContainerForSimpleTests.java
+++ trunk/transactions/src/test/java/test/com/hrodberaht/inject/extension/transaction/example/ModuleContainerForSimpleTests.java
@@ -3,8 +3,8 @@ package test.com.hrodberaht.inject.extension.transaction.example;
 import org.hrodberaht.inject.InjectContainer;
 import org.hrodberaht.inject.InjectionRegisterModule;
 
-public class ModuleContainerForSimpleTest implements com.hrodberaht.inject.extension.transaction.junit.InjectionContainerCreator {
-    public ModuleContainerForSimpleTest() {
+public class ModuleContainerForSimpleTests implements com.hrodberaht.inject.extension.transaction.junit.InjectionContainerCreator {
+    public ModuleContainerForSimpleTests() {
     }
 
     public InjectContainer createContainer() {
