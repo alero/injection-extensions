bbd820599e916e83df761a9dd24ade6ed55cef0c
robert.alexandersson@gmail.com <robert.alexandersson@gmail.com@81990c2f-af16-a326-1b66-97b0f14e0d32>
2010-10-26 17:04:00 +0000

    Moved extension package

---
+++ ejbunit/src/main/java/org/hrodberaht/inject/extension/tdd/spring/SpringContainerConfigBase.java
@@ -0,0 +1,12 @@
+package org.hrodberaht.inject.extension.tdd.spring;
+
+/**
+ * ¤Projectname¤
+ *
+ * @author Robert Alexandersson
+ *         2010-okt-26 18:59:45
+ * @version 1.0
+ * @since 1.0
+ */
+public class SpringContainerConfigBase {
+}
