6d787f22bacea0454681afe6e9aa3cbac9bc2f23
robert.alexandersson@gmail.com <robert.alexandersson@gmail.com@81990c2f-af16-a326-1b66-97b0f14e0d32>
2013-02-12 20:17:24 +0000

    JEEresource handling added

--- trunk/cdi-light/src/main/java/org/hrodberaht/inject/extension/cdi/inner/JEEResourceCreator.java
+++ trunk/cdi-light/src/main/java/org/hrodberaht/inject/extension/cdi/inner/JEEResourceCreator.java
@@ -2,10 +2,14 @@ package org.hrodberaht.inject.extension.cdi.inner;
 
 import org.hrodberaht.inject.spi.ResourceCreator;
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Created with IntelliJ IDEA.
@@ -16,18 +20,43 @@ import java.util.Collection;
  */
 public class JEEResourceCreator implements ResourceCreator<EntityManager, DataSource> {
 
+    private Map<String, DataSource> dataSources = new ConcurrentHashMap<String, DataSource>();
+    private InitialContext ctx;
+
+    public JEEResourceCreator() {
+        try {
+            ctx = new InitialContext();
+        } catch (NamingException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public DataSource createDataSource(String dataSourceName) {
-
-        return null;
+        DataSource dataSource = dataSources.get(dataSourceName);
+        if(dataSource == null){
+            try {
+                String appServer = System.getProperty("app.server");
+                if (appServer != null && appServer.equals("jboss")) {
+                    dataSource = (DataSource) ctx.lookup("java:/" + dataSourceName);
+                }else{
+                    dataSource = (DataSource) ctx.lookup(dataSourceName);
+                }
+            } catch (NamingException e) {
+                throw new RuntimeException(e);
+            }
+            if(dataSource == null){
+                throw new IllegalArgumentException("datasource not found"+dataSourceName);
+            }
+        }
+        return dataSource;
     }
 
     public boolean hasDataSource(String dataSourceName) {
-        return true;
+        return dataSources.get(dataSourceName) != null;
     }
 
     public DataSource getDataSource(String dataSourceName) {
-        return null;
+        return dataSources.get(dataSourceName);
     }
 
     public EntityManager createEntityManager(String schemaName, String dataSourceName, DataSource dataSource) {
