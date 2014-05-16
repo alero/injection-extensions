d82c2a67922a32358aa497b9c585052b82dd7e78
robert.alexandersson@gmail.com <robert.alexandersson@gmail.com@81990c2f-af16-a326-1b66-97b0f14e0d32>
2014-05-16 09:01:00 +0000

    Full support for jar files in all scan areaas, extended the SQL loading "protection" to enable inherited test modules

--- trunk/ejbunit/src/main/java/org/hrodberaht/inject/extension/tdd/internal/DataSourceExecution.java
+++ trunk/ejbunit/src/main/java/org/hrodberaht/inject/extension/tdd/internal/DataSourceExecution.java
@@ -1,6 +1,8 @@
 package org.hrodberaht.inject.extension.tdd.internal;
 
 
+import org.hrodberaht.inject.extension.tdd.util.JarUtil;
+import org.hrodberaht.inject.extension.tdd.util.SimpleLogger;
 import org.hrodberaht.inject.spi.ResourceCreator;
 
 import java.io.*;
@@ -9,6 +11,13 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 /**
  * Unit Test EJB (using @Inject)
@@ -32,15 +41,67 @@ public class DataSourceExecution {
 
     public void addSQLSchemas(String schemaName, String packageBase) {
 
-        URL url = Thread.currentThread().getContextClassLoader().getResource(packageBase);
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+
+
+        List<File> files = new ArrayList<File>();
+        files.addAll(findFiles(classLoader, packageBase));
+        runScripts(files, schemaName, SCHEMA_PREFIX);
+        runScripts(files, schemaName, INSERT_SCRIPT_PREFIX);
+
+
+        findJarFiles(classLoader, packageBase, schemaName);
+
+    }
+
+    private void  findJarFiles(ClassLoader classLoader, String packageBase, String schemaName) {
+
+        try {
+            List<File> filesToLoad = JarUtil.findTheJarFiles(packageBase, classLoader);
+
+
+            if(filesToLoad == null){
+                return;
+            }
+            for(File fileToLoad:filesToLoad){
+                SimpleLogger.log("findJarFiles fileToLoad = " + fileToLoad);
+                JarFile jarFile = new JarFile(fileToLoad);
+                Enumeration<JarEntry> enumeration = jarFile.entries();
+                while(enumeration.hasMoreElements()){
+                    JarEntry jarEntry = enumeration.nextElement();
+                    String jarName = jarEntry.getName();
+                    if(!jarEntry.isDirectory() && jarName.startsWith(packageBase)
+                            && jarName.endsWith(".sql")){
+
+                        java.io.InputStream is = jarFile.getInputStream(jarEntry);
+                        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+                        String strLine;
+                        StringBuffer stringBuffer = new StringBuffer();
+                        while ((strLine = br.readLine()) != null) {
+                            stringBuffer.append(strLine);
+                        }
+                        executeStringToSQL(schemaName, stringBuffer);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Bad file "+packageBase + " with classloader:"+classLoader);
+        }
+
+    }
+
+    private List<File> findFiles(ClassLoader classLoader, String packageBase) {
+        URL url = classLoader.getResource(packageBase);
         String directoryString = url.getFile().replaceAll("%20", " ");
         File directory = new File(directoryString);
         File[] files = directory.listFiles();
-        runScripts(files, schemaName, SCHEMA_PREFIX);
-        runScripts(files, schemaName, INSERT_SCRIPT_PREFIX);
+        if(files == null){
+            return new ArrayList<File>();
+        }
+        return Arrays.asList(files);
     }
 
-    private void runScripts(File[] files, String schemaName, String prefix) {
+    private void runScripts(List<File> files, String schemaName, String prefix) {
         for (File file : files) {
             if (file.isFile() && file.getName().startsWith(prefix)) {
                 executeScript(file, schemaName);
@@ -62,24 +123,7 @@ public class DataSourceExecution {
                 stringBuffer.append(strLine);
             }
 
-            DataSourceProxy dataSourceProxy = (DataSourceProxy) resourceCreator.getDataSource(schemaName);
-            Statement stmt = null;
-            try {
-                Connection connection = dataSourceProxy.getConnection();
-                stmt = connection.createStatement();
-                stmt.execute(stringBuffer.toString());
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            } finally {
-                try {
-                    if (stmt != null) {
-                        stmt.close();
-                    }
-                } catch (SQLException e) {
-
-                }
-                dataSourceProxy.commitDataSource();
-            }
+            executeStringToSQL(schemaName, stringBuffer);
 
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
@@ -88,8 +132,33 @@ public class DataSourceExecution {
         }
     }
 
+    private void executeStringToSQL(String schemaName, StringBuffer stringBuffer) {
+        DataSourceProxy dataSourceProxy = (DataSourceProxy) resourceCreator.getDataSource(schemaName);
+        Statement stmt = null;
+        try {
+            Connection connection = dataSourceProxy.getConnection();
+            stmt = connection.createStatement();
+            stmt.execute(stringBuffer.toString());
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (stmt != null) {
+                    stmt.close();
+                }
+            } catch (SQLException e) {
+
+            }
+            dataSourceProxy.commitDataSource();
+        }
+    }
+
 
     public boolean isInitiated(String testPackageName, String schemaName) {
+        return this.isInitiated(testPackageName, schemaName, testPackageName);
+    }
+
+    public boolean isInitiated(String testPackageName, String schemaName, String initiatedTableName) {
         DataSourceProxy dataSourceProxy = (DataSourceProxy) resourceCreator.getDataSource(schemaName);
         PreparedStatement pstmt = null;
         try {
@@ -97,7 +166,7 @@ public class DataSourceExecution {
                 throw new IllegalAccessError("schemaName:"+schemaName +" does not exist ");
             }
             Connection connection = dataSourceProxy.getConnection();
-            pstmt = connection.prepareStatement("create table "+testPackageName+" (  control_it integer)");
+            pstmt = connection.prepareStatement("create table "+testPackageName+initiatedTableName+" (  control_it integer)");
             pstmt.execute();
             dataSourceProxy.commitDataSource();
             return false;
